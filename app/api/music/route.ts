6349d1d0e2cba07a45707aeb639ea674e5bee8a2
Buddhi Ashen <buddhiict@gmail.com>
2023-07-29 20:54:23 +0530

    Update route.ts

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -36,7 +36,7 @@ export async function POST(
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
       {
         input: {
-          prompt_a: "funky synth solo"
+          prompt_a: "piano"
         }
       }
     );
