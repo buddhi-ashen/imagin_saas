abdc4a8be33290f337b2dec58be227a44366e129
Buddhi Ashen <buddhiict@gmail.com>
2023-07-30 07:16:37 +0530

    Update route.ts

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -32,15 +32,14 @@ export async function POST(
       return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
     }
 
-   const output = await replicate.run(
-  "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
-  {
-    input: {
-      prompt_a: "funky synth solo"
-    }
-  }
-);
-      
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt
+        }
+      }
+    );
 
     if (!isPro) {
       await incrementApiLimit();
